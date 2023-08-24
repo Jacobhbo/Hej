5ab63fc707107ee4ae308b448bf6c2454b95e409
Tec <Tec@SKAB2-PC7>
2023-08-24 08:44:35 +0100

    Add project files.

---
+++ CaseTec/Code/Elev.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecTilmeldning.Code
+{
+    internal sealed class Elev : Person
+    {
+
+        public Elev(string? fornavn, string? efternavn, DateTime fødselsdag) : base(fornavn, efternavn, fødselsdag)
+        {
+            string defaultInfo = ShowAllInfo2();
+        }
+
+        protected override string ShowAllInfo()
+        {
+            return $"{PersonInfo.Fornavn} {PersonInfo.Efternavn}";
+        }
+
+    }
+}
---
+++ CaseTec/Køretøjer/Bil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecTilmeldning.Køretøjer
+{
+    internal sealed class Bil : Kørtøj
+    {
+        public double MotorStørrelse { get; set; }
+
+
+        public Bil(string? mærke, string? model, double motorStørrelse) : base(mærke, model)
+        {
+            MotorStørrelse = motorStørrelse;
+        }
+    }
+}
